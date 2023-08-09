20923f4373a304d1166ae53acc51740959e91a11
vikasprajapati-01 <vikasprajapati9980@gmail.com>
2023-08-09 21:03:29 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function showMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.style.right = "-200px";
+}
